1c71ef71232b19c52ef46e81b35a703677e029d1
ngan <vutran@sheridancollege.ca>
2024-06-08 18:08:23 -0400

    Clean Code

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,9 +2,7 @@ const Blog = require('../models/blog');
 
 function blog_index(req, res) {
     Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-        res.render("index", { title: "Home", blogs: result });
-        })
+        .then(result => res.render("index", { title: "Home", blogs: result }))
         .catch((err) => res.send(err));
 }
 
@@ -15,26 +13,22 @@ function blog_create_get(req, res) {
 function blog_details(req, res) {
     const id = req.params.id;
     Blog.findById(id)
-        .then((result) => res.render("blog-detail", { title: "Blog Details", blog: result }))
-        .catch((err) => res.send(err));
+        .then(result => res.render("blog-detail", { title: "Blog Details", blog: result }))
+        .catch(err => res.status(404).render('404', { title: '404' }));
 }
 
 function blog_delete(req, res) {
     const id = req.params.id
     Blog.findByIdAndDelete(id)
-        .then((result) => res.json({ redirect: "/" }))
-        .catch((err) => console.log(err))
+        .then(result => res.json({ redirect: "/" }))
+        .catch(err => console.log(err))
 }
 
 function blog_create_post(req, res) {
     const blog = new Blog(req.body);
     blog.save()
-        .then((result) => {
-            res.redirect("/blogs/create");
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        .then(result => res.redirect("/blogs/create"))
+        .catch(err => console.log(err))
 }
 
 module.exports = {
--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -1,22 +1,21 @@
 const express = require("express")
 const router = express.Router()
-const Blog = require('../models/blog');
 const blogController = require("../controllers/blogController")
 
 // Home Page
-router.get("/", blogController.blog_index);
+router.get("/", blogController.blog_index)
 
 // Create Page
-router.get("/create", blogController.blog_create_get);
+router.get("/create", blogController.blog_create_get)
 
 // Create Blog (POST) --> Need a middleware to do .body
 router.post("/", blogController.blog_create_post)
 
 // Get One Blog (GET)
-router.get("/:id", blogController.blog_details);
+router.get("/:id", blogController.blog_details)
 
 // Delete Blog (DELETE)
-router.delete("/:id", blogController.blog_delete);
+router.delete("/:id", blogController.blog_delete)
 
 // Export Router 
 module.exports = router
